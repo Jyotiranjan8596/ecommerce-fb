38ace4c08b9e72de5dd8c69625ae50037d22347b
unknown <sahoorinku63@gmail.com>
2025-09-22 23:03:05 +0530

    add to home screen icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,14 +36,14 @@
     <meta property="twitter:image"
         content="https://themesberg.s3.us-east-2.amazonaws.com/public/products/volt-pro-bootstrap-5-dashboard/volt-pro-preview.jpg" />
 
-   <!-- Favicon -->
-   <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/images/faviconfreebazar.png') }}" />
-   <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/faviconfreebazar.png') }}" />
-   <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/faviconfreebazar.png') }}" />
-   <link rel="icon" href="{{ asset('assets/images/faviconfreebazar.png') }}" />
-   <link rel="mask-icon" href="{{ asset('assets/images/faviconfreebazar.png') }}" color="#ffffff" />
-   <meta name="msapplication-TileColor" content="#ffffff" />
-   <meta name="theme-color" content="#ffffff" />
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('assets/images/faviconfreebazar.png') }}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/faviconfreebazar.png') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/faviconfreebazar.png') }}" />
+    <link rel="icon" href="{{ asset('assets/images/faviconfreebazar.png') }}" />
+    <link rel="mask-icon" href="{{ asset('assets/images/faviconfreebazar.png') }}" color="#ffffff" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="theme-color" content="#ffffff" />
 
     <!-- Sweet Alert -->
     <link type="text/css" href="{{ asset('vendor/sweetalert2/dist/sweetalert2.min.css') }}" rel="stylesheet" />
