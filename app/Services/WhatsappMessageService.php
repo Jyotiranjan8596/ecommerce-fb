eab2dcfa3c2af7b33793ca63bf90041d6ba97cba
Jyotiranjan Sahoo <sahoorinku63@gmail.com>
2025-12-31 21:26:42 +0530

    update

--- app/Services/WhatsappMessageService.php
+++ app/Services/WhatsappMessageService.php
@@ -13,7 +13,7 @@ class WhatsappMessageService
         try {
             $token = env('WHATSAPP_TOKEN');
             $phoneNumberId = env('WHATSAPP_PHONE_NUMBER_ID');
-            $to = '919336801361'; // Verified test number
+            $to = '7978910379'; // Verified test number
 
             $url = "https://graph.facebook.com/v22.0/886143784580503/messages";
             $template = 'hello_world';
